fd928fcfcaa0ba878cc225a5f2c1a00600e4decc
cho45 (さとう) <cho45@lowreal.net>
2011-06-15 01:23:22 +0000

    Import of SATOH/Test-Name-FromLine-0.01 from CPAN.
    gitpan-cpan-distribution: Test-Name-FromLine
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SATOH/Test-Name-FromLine-0.01.tar.gz
    gitpan-cpan-author:       SATOH
    gitpan-cpan-maturity:     released

---
+++ lib/Test/Name/FromLine.pm
@@ -0,0 +1,64 @@
+package Test::Name::FromLine;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use Test::Builder;
+use File::Slurp;
+
+no warnings 'redefine';
+my $ORIGINAL_ok = \&Test::Builder::ok;
+*Test::Builder::ok = sub {
+	my %filecache;
+	$_[2] ||= do {
+		my ($package, $filename, $line) = caller($Test::Builder::Level);
+		my $file = $filecache{$filename} ||= [ read_file($filename) ];
+		my $lnum = $line;
+		$line = $file->[$lnum-1];
+		$line =~ s{^\s+|\s+$}{}g;
+		"L$lnum: $line";
+	};
+	goto &$ORIGINAL_ok;
+};
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Test::Name::FromLine - Auto fill test names from caller line
+
+=head1 SYNOPSIS
+
+  use Test::Name::FromLine; # just use this
+  use Test::More;
+
+  is 1, 1; #=> ok 1 - L3: is 1, 1;
+
+  done_testing;
+
+
+=head1 DESCRIPTION
+
+Test::Name::FromLine is test utility that fills test names from its file.
+Just use this module in test and this module fill test names to all test except named one.
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+This is inspired from L<http://subtech.g.hatena.ne.jp/motemen/20101214/1292316676>.
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
