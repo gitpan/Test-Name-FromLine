06112565dedcdd1fd658e8a50ad2f534e5ad925c
cho45 (さとう) <cho45@lowreal.net>
2014-05-16 14:36:31 +0000

    Import of SATOH/Test-Name-FromLine-0.13 from CPAN.
    gitpan-cpan-distribution: Test-Name-FromLine
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         SATOH/Test-Name-FromLine-0.13.tar.gz
    gitpan-cpan-author:       SATOH
    gitpan-cpan-maturity:     released

--- lib/Test/Name/FromLine.pm
+++ lib/Test/Name/FromLine.pm
@@ -3,7 +3,7 @@ package Test::Name::FromLine;
 use strict;
 use warnings;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 use Test::Builder;
 use File::Slurp;
@@ -19,7 +19,7 @@ my $ORIGINAL_ok = \&Test::Builder::ok;
 	@_ = @_; # for pass and fail
 	$_[2] = do {
 		my ($package, $filename, $line) = caller($Test::Builder::Level);
-		undef $filename if $filename eq '-e';
+		undef $filename if $filename && $filename eq '-e';
 		if ($filename) {
 			$filename = File::Spec->rel2abs($filename, $BASE_DIR);
 			my $file = $filecache{$filename} ||= [ read_file($filename) ];
